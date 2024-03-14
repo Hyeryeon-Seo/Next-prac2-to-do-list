bb20fe7f4f29e9f9ca1a6b3c981cdafd8c6e613a
Hyeryeon-Seo <0168968060@naver.com>
2024-03-14 23:04:31 +0900

    feat: route handler -PATCH 함수 추가

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -24,6 +24,19 @@ export async function POST(request: Request) {
 	return Response.json({ todo });
 }
 
+export async function PATCH(request: Request) {
+	const { id, isDone } = await request.json();
+	const response = await fetch(`http://localhost:4000/todos/${id}`, {
+		method: "PATCH",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ isDone: !isDone }),
+	});
+	const todo = await response.json();
+	return Response.json({ todo });
+}
+
 export async function DELETE(request: Request) {
 	const id = await request.json();
 	const response = await fetch(`http://localhost:4000/todos/${id}`, {
