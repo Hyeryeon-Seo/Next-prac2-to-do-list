8ff8208f078d2fcf7636c39b5ec137efd9caacf6
Hyeryeon-Seo <0168968060@naver.com>
2024-03-14 23:01:38 +0900

    add: not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 
 const NotFoundPage = () => {
-	return (
-		<div>
-			<p>잘못된 접근입니다. 이 페이지는 존재하지 않습니다.</p>
-			<p>삭제되었거나 잘못된 주소입니다.</p>
-		</div>
-	);
+  return (
+    <div className="flex flex-col justify-center h-svh items-center font-semibold text-xl leading-loose">
+      <p>😥❓</p>
+      <p>⚠️ 잘못된 접근입니다. 이 페이지는 존재하지 않습니다. ❌</p>
+      <p>삭제되었거나 잘못된 주소입니다.</p>
+    </div>
+  );
 };
 
 export default NotFoundPage;
