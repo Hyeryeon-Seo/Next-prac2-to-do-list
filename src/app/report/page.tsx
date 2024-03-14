f9248ab87c0453a62c5a3a3ebe928ae63cf04d2e
Hyeryeon-Seo <0168968060@naver.com>
2024-03-14 23:04:49 +0900

    feat: report page 필요기능 구현완료

--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -3,7 +3,7 @@ import type { Todo } from "@/types";
 // ISR (10초 주기)
 const ReportPage = async () => {
 	const response = await fetch(`http://localhost:4000/todos`, {
-		// 통계정보는 json-server에 만들기
+		// 서버컴포넌트 바로 json-server와 통신
 		next: {
 			revalidate: 10,
 		},
@@ -20,9 +20,62 @@ const ReportPage = async () => {
 	return (
 		<div>
 			{/* TODO 간략하게 working , done todos 보여주기 (표로?) */}
-			<p>현재 총 {totalNumTodos}개의 할 일 목록이 등록되어 있어요!</p>
-			<p>현재 {totalNumInProgressTodos}개의 할 일이 남아있어요</p>
-			<p>현재까지 {totalNumDoneTodos}개의 할 일을 완료했어요</p>
+			<div className="flex flex-col items-center m-20">
+				<h1 className="font-bold text-3xl mb-3">My To-Do Report</h1>
+				<p>You can see a report about your to-dos </p>
+
+				<section className="mt-20 mb-20 flex flex-col gap-5">
+					<p>
+						📌 현재 총 <b>{totalNumTodos}</b>개의 할 일 목록이 등록되어 있어요!
+						📋
+					</p>
+					<p>
+						🏃‍♀️ 현재 <b>{totalNumInProgressTodos}</b>개의 할 일이 남아있어요
+					</p>
+					<p>
+						🥰 현재까지 <b>{totalNumDoneTodos}</b>개의 할 일을 완료했어요
+					</p>
+				</section>
+
+				{/* table padding p- 안먹힘.. */}
+				{/* overflow-hidden 중요 : 해야 둥글기 적용됨 */}
+				<table
+					className="table-auto text-lg border-2 w-full border-gray-400/30 rounded-[15px] overflow-hidden 
+				text-center bg-[#504b4b] text-[#fff] shadow-md shadow-slate-800"
+				>
+					<thead className="h-[4rem]">
+						<tr className="bg-[rgba(224,189,189,0.66)]  ">
+							<th className="border-r-2 border-gray-600/30 w-10">No.</th>
+							<th className="border-r-2 border-gray-600/30">제목</th>
+							<th className="border-r-2 border-gray-600/30">내용</th>
+							<th className="w-1/12">완료 상태</th>
+						</tr>
+					</thead>
+					<tbody>
+						{todos.map((todo: Todo, index: number) => {
+							return (
+								<tr
+									key={todo.id}
+									className="border-b-2 border-b-gray-400/30 h-[4rem]"
+								>
+									<td className="border-r-2 border-r-gray-400/30">
+										{index + 1}
+									</td>
+									<td className="border-r-2 border-r-gray-400/30">
+										{todo.title}
+									</td>
+									<td className="border-r-2 border-r-gray-400/30">
+										{todo.contents}
+									</td>
+									{/* overflow-hidden  trancate? ellipsis 다 적용해봤지만 길어지면 그냥 줄바꿈..
+									넓이, 길이도 설정해봤으나 안됨*/}
+									<td>{todo.isDone ? "완료 🎉" : "진행 중 🏃‍♀️"}</td>
+								</tr>
+							);
+						})}
+					</tbody>
+				</table>
+			</div>
 		</div>
 	);
 };
