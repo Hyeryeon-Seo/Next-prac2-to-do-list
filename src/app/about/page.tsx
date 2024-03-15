be5059107226e038fe0f515f54e987830b301ec8
Hyeryeon-Seo <0168968060@naver.com>
2024-03-15 10:16:01 +0900

    update: about page 약간 수정

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,27 +1,21 @@
-import React from "react";
+import React from 'react';
 
 // SSG
 const AboutPage = async () => {
-	const response = await fetch(`http://localhost:4000/companyInfo`, {
-		// 서버 컴포넌트라 바로 json-server DB 통신
-		cache: "force-cache",
-	});
+    const response = await fetch(`http://localhost:4000/companyInfo`, {
+        // 서버 컴포넌트라 바로 json-server DB 통신
+        cache: 'force-cache'
+    });
+    const companyInfo = await response.json();
+    const { image, name, description } = companyInfo;
 
-	const companyInfo = await response.json(); // {name: .., description:.., image:..}
-	return (
-		<div className="flex flex-col items-center m-20 gap-20">
-			<img
-				src={companyInfo.image}
-				alt="company-note-img"
-				width={500}
-				className="rounded"
-			/>
-			<p className="font-bold text-lg bg-rose-600/10 rounded p-3">
-				🏢 {companyInfo.name}
-			</p>
-			<p className="pl-56 pr-52 font-bold">{companyInfo.description}</p>
-		</div>
-	);
+    return (
+        <div className="m-20 flex flex-col items-center gap-20">
+            <img src={image} alt="company-note-img" width={500} className="rounded" />
+            <p className="rounded bg-rose-600/10 p-3 text-lg font-bold">🏢 {name}</p>
+            <p className="pl-56 pr-52 font-medium">{description}</p>
+        </div>
+    );
 };
 
 export default AboutPage;
