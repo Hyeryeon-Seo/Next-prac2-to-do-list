bc946628719a14c83daa3e726bd4a4c951505a0e
Hyeryeon-Seo <0168968060@naver.com>
2024-03-15 13:04:43 +0900

    chore: 주석 삭제

--- src/hooks/TodoList.hooks.ts
+++ src/hooks/TodoList.hooks.ts
@@ -1,11 +1,9 @@
 // 'use client';
-import React from 'react';
 import useToggleDoneMutation from './mutations/useToggleDoneMutation';
 import useDeleteTodoMutation from './mutations/useDeleteTodoMutation';
 import useAddTodoMutation from './mutations/useAddTodoMutation';
 
 const useTodo = () => {
-    // NOTE 이걸 실행시켜서 useMutation도 하고 (백엔드-DB업뎃), handle..함수를 통해 화면업뎃렌더링도 함
     const handleToggleTodoDone = useToggleDoneMutation();
     const handleDeleteTodo = useDeleteTodoMutation();
     const handleSubmitTodo = useAddTodoMutation();
